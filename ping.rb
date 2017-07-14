e983160ee692d629bcfd16c64df410c9e392c7db
Paeladon <MaxwellDipper@gmail.com>
2017-07-14 20:59:04 +0200

    Update ping.rb

--- ping.rb
+++ ping.rb
@@ -1,6 +1,6 @@
 require 'discordrb'
 
-bot = Discordrb::Bot.new token: MzM1MDQ5NTEzNzAwMTYzNTg1.DEqgDA.NMZ6qxWicvjZl_MPFds8romveAU, client_id: 335049513700163585
+bot = Discordrb::Bot.new token: WIlu2cFjgameNdA70Vnn4v9smg81G6PNCPgOh3VI3HKc_f8qZl8gZ4oDxbYAM0TiUeGN, client_id: 335049513700163585
 
 bot.message(content: 'Ping!') do |event|
   event.respond 'Pong!'
