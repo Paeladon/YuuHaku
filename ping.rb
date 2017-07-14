7b12182007b7ce2f2eb126741d71037447356f9c
Paeladon <MaxwellDipper@gmail.com>
2017-07-14 20:50:31 +0200

    Update ping.rb

--- ping.rb
+++ ping.rb
@@ -1,6 +1,6 @@
 require 'discordrb'
 
-bot = Discordrb::Bot.new token: MzM1MDQ5NTEzNzAwMTYzNTg1.DEoW-w.OXmZrGssoOfRXJeFk-qqxrm5WZY, client_id: 168123456789123456
+bot = Discordrb::Bot.new token: MzM1MDQ5NTEzNzAwMTYzNTg1.DEoW-w.OXmZrGssoOfRXJeFk-qqxrm5WZY, client_id: 335049513700163585
 
 bot.message(content: 'Ping!') do |event|
   event.respond 'Pong!'
