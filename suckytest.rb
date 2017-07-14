5c0f43e39fa87e2d599ded657ba3d443aa854e96
Paeladon <MaxwellDipper@gmail.com>
2017-07-14 15:55:01 +0200

    Update suckytest.rb

--- suckytest.rb
+++ suckytest.rb
@@ -2,7 +2,7 @@ require 'discordrb'
 
 bot = Discordrb::Bot.new token: MzM1MDQ5NTEzNzAwMTYzNTg1.DEoW-w.OXmZrGssoOfRXJeFk-qqxrm5WZY, client_id: 168123456789123456
 
-bot.message(with_text: 'Ping!') do |event|
+bot.message(content: 'Ping!') do |event|
   event.respond 'Pong!'
 end
 
